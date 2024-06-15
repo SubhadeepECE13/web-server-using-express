57984c91c4841dde5150dc5981b4f4c24491a3c9
SubhadeepECE13 <160423584+SubhadeepECE13@users.noreply.github.com>
2024-06-15 09:02:33 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const port = 5500;
+console.log("hello world")
+
+app.listen(port, () => {
+	console.log(`Success! Your application is running on port ${port}.`);
+});
